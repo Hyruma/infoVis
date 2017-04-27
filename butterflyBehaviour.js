d56ea0de6f26058c807d6c52a979d412b646a2a4
Valerio Gregori <valrim992@gmail.com>
2017-04-27 23:01:05 +0200

    Add files via upload

--- butterflyBehaviour.js
+++ butterflyBehaviour.js
@@ -21,154 +21,133 @@ function behaviour(){
 		e = e || window.event;
 
 		var img= d3.select("image")
+		var y= parseInt(img.attr("transY"));
+		var x= parseInt(img.attr("transX"));
+		var z= parseInt(img.attr("orientation"));
+		var w= parseInt(img.attr("width"))/2;
+		var h= parseInt(img.attr("height"))/2;
+		var done= false;
+
 		if(e.keyCode == '40')
-			moveDown(img);
+			moveDown();
 		else if(e.keyCode == '38')
-			moveUp(img);
+			moveUp();
 		else if(e.keyCode == '39')
-			moveRight(img);
+			moveRight();
 		else if(e.keyCode == '37')
-			moveLeft(img);
-	}
+			moveLeft();
 
-	function moveUp(img){
-		var y= parseInt(img.attr("transY"));
-		var x= parseInt(img.attr("transX"));
-		var z= parseInt(img.attr("orientation"));
-		var w= parseInt(img.attr("width"))/2;
-		var h= parseInt(img.attr("height"))/2;
-
-		if (y<0){
-			y = maxH -speed + y;
-			img.attr("transform", "translate("+x+","+y+") rotate("+z+")");
-		} else {
-		if (z==0)
-			y-= speed;
-		else
-			z=0;
-		img.transition().duration("100").attr("transform", "translate("+x+","+y+") rotate("+z+","+w+","+h+")");
+		if(!done) {
+			img.transition()
+			   .duration("100")
+			   .attr("transform", move());
 		}
-		img.attr("transY", y);
-		img.attr("orientation", z);
-	}
-
-	function moveDown(img){
-		var y= parseInt(img.attr("transY"));
-		var x= parseInt(img.attr("transX"));
-		var z= parseInt(img.attr("orientation"));
-		var w= parseInt(img.attr("width"))/2;
-		var h= parseInt(img.attr("height"))/2;
-
-		if (y>maxH){
-			y = speed + y -maxH;
-			img.attr("transform", "translate("+x+","+y+") rotate("+z+")");
-		} else {
-		if (z==180)
-			y+= speed;
-		else {
-			z=180;
+	
+		function moveUp(){
+			if(z==0){
+				if (y<0){
+				y = maxH -speed + y;
+				img.attr("transform", move());
+				done= true;
+				} else
+					y-=speed;
+			} else
+				z=0;
+			img.attr("transY", y);
+			img.attr("orientation", z);
 		}
-		img.transition().duration("100").attr("transform", "translate("+x+","+y+") rotate("+z+","+w+","+h+")");
+
+		function moveDown(){
+			if(z==180){
+				if (y>maxH-h*2){
+				y = speed + y -maxH;
+				img.attr("transform", move());
+				done= true;
+				} else
+					y+=speed;
+			} else
+				z=180;
+			img.attr("transY", y);
+			img.attr("orientation", z);
 		}
-		img.attr("transY", y);
-		img.attr("orientation", z);
-	}
 
-	function moveRight(img){
-		var y= parseInt(img.attr("transY"));
-		var x= parseInt(img.attr("transX"));
-		var z= parseInt(img.attr("orientation"));
-		var w= parseInt(img.attr("width"))/2;
-		var h= parseInt(img.attr("height"))/2;
-
-		if (x>maxW){
-			x = speed + x -maxW;
-			img.attr("transform", "translate("+x+","+y+") rotate("+z+")");
-		} else {
-		if (z==90)
-			x+= speed;
-		else
-			z=90;
-		img.transition().duration("100").attr("transform", "translate("+x+","+y+") rotate("+z+","+w+","+h+")");
+		function moveRight(){
+			if(z==90){
+				if (x>maxW-w*2){
+				x = speed + x -maxW;
+				img.attr("transform", move());
+				done= true;
+				} else
+					x+= speed;
+			} else
+				z=90;
+			img.attr("transX", x);
+			img.attr("orientation", z);
 		}
-		img.attr("transX", x);
-		img.attr("orientation", z);
-	}
 
-	function moveLeft(img){
-		var y= parseInt(img.attr("transY"));
-		var x= parseInt(img.attr("transX"));
-		var z= parseInt(img.attr("orientation"));
-		var w= parseInt(img.attr("width"))/2;
-		var h= parseInt(img.attr("height"))/2;
-
-		if (x<0){
-			x = maxW + x -speed;
-			img.attr("transform", "translate("+x+","+y+") rotate("+z+")");
-		} else {
-		if (z==270)
-			x-= speed;
-		else
-			z=270;
-		img.transition().duration("100").attr("transform", "translate("+x+","+y+") rotate("+z+","+w+","+h+")");
+		function moveLeft(){
+			if(z==270){
+				if (x<0){
+				x = maxW + x -speed;
+				img.attr("transform", move());
+				done= true;
+				} else
+					x-= speed;
+			} else
+				z=270;
+			img.attr("transX", x);
+			img.attr("orientation", z);
 		}
-		img.attr("transX", x);
-		img.attr("orientation", z);
+
+		function move(){
+			return "translate("+x+","+y+") rotate("+z+","+w+","+h+")";
+		}
 	}
 
 
-document.getElementById("butterfly").onclick = function(e){
-				e = e || window.event;
+	document.getElementById("butterfly").onclick = function(e){
+		e = e || window.event;
 
-				var radius= maxW;
-				var angle = Math.random()*360;
-				var angleR= angle * (Math.PI / 180);
-				var img = d3.select("image");
-				var x= parseInt(img.attr("transX"))
-				var y= parseInt(img.attr("transY"))
-				var outX=  (radius * Math.cos(angleR));
-				var outY=  (radius * Math.sin(angleR));
-			img.attr("xlink:href", "ikshi").
-			img.transition().duration("2000")
-			.attr("transform", "translate("+x+","+y+") rotate("+(90-angle)+",50,50)")
+		var radius= maxW;
+		var angle = Math.random()*360;
+		var angleR= angle * (Math.PI / 180);
+		var img = d3.select("image");
+		var x= parseInt(img.attr("transX"))
+		var y= parseInt(img.attr("transY"))
+		var outX=  (radius * Math.cos(angleR));
+		var outY=  (radius * Math.sin(angleR));
 
-			img.transition().duration("5000").delay(2000)
-			.attr("transform", "translate("+(x+outX)+","+(y-outY)+") rotate("+(90-angle)+",50,50)")
-			//img.attr("xlink:href" "ikshi").
-			.each("end", function(){
+		//img.attr("xlink:href", "ikshi").
+		img.transition().duration("2000")
+		.attr("transform", "translate("+x+","+y+") rotate("+(90-angle)+",50,50)")
 
-			e.target.parentNode.removeChild(element); })
+		var tot= 5000/30;
+		var i=0;
+		var vx= outX/tot;
+		var vy= outY/tot;
+		var flipped= false;
+
+		var refreshIntervalId = setInterval(repeatMe,80);
+
+		function repeatMe(){
+			x= x + vx;
+			y= y - vy;
+			if(flipped)
+				img.attr("xlink:href", "images/butterfly.svg")
+			else 
+				img.attr("xlink:href", "images/butterflyFlipped.svg")
+
+			flipped= !flipped;
+			img.transition().duration("10").delay(2000)
+			.attr("transform", "translate("+(x+vx)+","+(y-vy)+") rotate("+(90-angle)+",50,50)")
+
+			i++;
+
+			if(i>tot){
+				clearInterval(refreshIntervalId);
+				e.target.parentNode.removeChild(e.target);
+			}
+		}
 	}
-
-
-	var tot= 5000/20;
-	var i=0;
-	var vx= outX/tot;
-	var vy= ouY/tot;
-	function repeatMe()
-	            {
-	                x = x + vx;
-	                y = y - vy;
-
-	                ball.transition()
-	                    .duration(10).attr("transform",function(k){
-	                    return "translate("+x+","+y+")";
-	                });
-
-	                i=i+0.2;
-
-	                if(i>t){
-	                    clearInterval(refreshIntervalId);
-	                    ball.transition().delay(100)
-	                        .duration(1000).attr("transform",function(k){
-	                        return "translate("+cannonPosX+","+cannonPosY+")";
-
-	                    });
-	                    lock=!lock;
-
-
-	                }
-	            }
-	            var refreshIntervalId = setInterval(repeatMe,20);
-        }
 }
+
