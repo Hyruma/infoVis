f7fada05de5bbde42199f8ac6029d9b2f137fb1f
Valerio Gregori <valrim992@gmail.com>
2017-04-28 12:06:34 +0200

    Final commit

--- butterflyBehaviour.js
+++ butterflyBehaviour.js
@@ -1,58 +1,62 @@
-
-function behaviour(){
-
 	var speed= 40;
 	var maxW = document.body.clientWidth;
 	var maxH = document.body.scrollHeight;
+	var clicked= false;
+	init();
 
-	var svg= d3.select("svg");
+	function init() {
 
-	var img = svg.append("svg:image")
-	    .attr("xlink:href", "images/butterfly.svg")
-	    .attr("id", "butterfly")
-	    .attr("width", 100)
-	    .attr("height", 100)
-	    .attr("transX", (maxW*90)/200)
-	    .attr("transY", maxH/2)
-	    .attr("orientation", 0)
-	    .attr("transform", "translate("+(maxW*90)/200+","+maxH/2+") rotate(0)" );
+		var myNode = document.getElementById("svg");
+		if (myNode.firstChild) {
+			myNode.removeChild(myNode.firstChild);
+		}
+
+		var svg= d3.select("svg");
+		var img = svg.append("svg:image")
+			.attr("xlink:href", "images/butter.svg")
+			.attr("id", "butterfly")
+			.attr("width", 100)
+			.attr("height", 100)
+			.attr("transX", (maxW*90)/200)
+			.attr("transY", maxH/2)
+			.attr("orientation", 0)
+			.attr("transform", "translate("+(maxW*90)/200+","+maxH/2+") rotate(0)" );
+	}
 
 	document.onkeydown = function(e){
-		e = e || window.event;
+		if(!clicked){
+			e = e || window.event;
 
-		var img= d3.select("image")
-		var y= parseInt(img.attr("transY"));
-		var x= parseInt(img.attr("transX"));
-		var z= parseInt(img.attr("orientation"));
-		var w= parseInt(img.attr("width"))/2;
-		var h= parseInt(img.attr("height"))/2;
-		var done= false;
+			var img= d3.select("image")
+			var duration = 100;
+			var y= parseInt(img.attr("transY"));
+			var x= parseInt(img.attr("transX"));
+			var z= parseInt(img.attr("orientation"));
+			var w= parseInt(img.attr("width"))/2;
+			var h= parseInt(img.attr("height"))/2;
 
-		if(e.keyCode == '40')
-			moveDown();
-		else if(e.keyCode == '38')
-			moveUp();
-		else if(e.keyCode == '39')
-			moveRight();
-		else if(e.keyCode == '37')
-			moveLeft();
-
-		if(!done) {
-			img.transition()
-			   .duration("100")
-			   .attr("transform", move());
+			if(e.keyCode == '40')
+				moveDown();
+			else if(e.keyCode == '38')
+				moveUp();
+			else if(e.keyCode == '39')
+				moveRight();
+			else if(e.keyCode == '37')
+				moveLeft();
 		}
-	
+
 		function moveUp(){
 			if(z==0){
 				if (y<0){
 				y = maxH -speed + y;
 				img.attr("transform", move());
-				done= true;
 				} else
 					y-=speed;
-			} else
+			} else {
+				duration= 500;
 				z=0;
+			}
+			img.transition().duration(duration).attr("transform", move());
 			img.attr("transY", y);
 			img.attr("orientation", z);
 		}
@@ -62,11 +66,13 @@ function behaviour(){
 				if (y>maxH-h*2){
 				y = speed + y -maxH;
 				img.attr("transform", move());
-				done= true;
 				} else
 					y+=speed;
-			} else
+			} else {
+				duration= 500;
 				z=180;
+			}
+			img.transition().duration(duration).attr("transform", move());
 			img.attr("transY", y);
 			img.attr("orientation", z);
 		}
@@ -76,11 +82,13 @@ function behaviour(){
 				if (x>maxW-w*2){
 				x = speed + x -maxW;
 				img.attr("transform", move());
-				done= true;
 				} else
 					x+= speed;
-			} else
+			} else {
+				duration=500;
 				z=90;
+			}
+			img.transition().duration(duration).attr("transform", move());
 			img.attr("transX", x);
 			img.attr("orientation", z);
 		}
@@ -90,11 +98,13 @@ function behaviour(){
 				if (x<0){
 				x = maxW + x -speed;
 				img.attr("transform", move());
-				done= true;
 				} else
 					x-= speed;
-			} else
+			} else {
+				duration= 500;
 				z=270;
+			}
+			img.transition().duration(duration).attr("transform", move());
 			img.attr("transX", x);
 			img.attr("orientation", z);
 		}
@@ -106,6 +116,8 @@ function behaviour(){
 
 
 	document.getElementById("butterfly").onclick = function(e){
+		if(!clicked){
+			clicked= true;
 		e = e || window.event;
 
 		var radius= maxW;
@@ -121,7 +133,7 @@ function behaviour(){
 		img.transition().duration("2000")
 		.attr("transform", "translate("+x+","+y+") rotate("+(90-angle)+",50,50)")
 
-		var tot= 5000/30;
+		var tot= 5000/40;
 		var i=0;
 		var vx= outX/tot;
 		var vy= outY/tot;
@@ -133,9 +145,9 @@ function behaviour(){
 			x= x + vx;
 			y= y - vy;
 			if(flipped)
-				img.attr("xlink:href", "images/butterfly.svg")
-			else 
-				img.attr("xlink:href", "images/butterflyFlipped.svg")
+				img.attr("xlink:href", "images/butter.svg")
+			else
+				img.attr("xlink:href", "images/butterFlipped.svg")
 
 			flipped= !flipped;
 			img.transition().duration("10").delay(2000)
